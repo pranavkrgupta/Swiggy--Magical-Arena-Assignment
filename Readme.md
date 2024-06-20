99f9dd97495af7e82e1927584a25450243f79461
Pranav Kumar Gupta <pranavkumar2998@gmail.com>
2024-06-20 14:52:35 +0530

    Add Comments

--- Readme.md
+++ Readme.md
@@ -20,7 +20,11 @@ npm test
 
 **Problem Statement:**
 
-Design a Magical Arena. Every Player is defined by a “health” attribute, “strength” attribute and an “attack” attribute - all positive integers. The player dies if his health attribute touches 0.
+Design a Magical Arena.
+
+Every Player is defined by 3 attributes - Health, Strength, Attack - all positive integers.
+
+The player dies if his health attribute touches 0.
 Any two player can fight a match in the arena. Players attack in turns. Attacking player rolls the attacking dice and the defending player rolls the defending dice. The “attack” value multiplied by the outcome of the attacking dice roll is the damage created by the attacker. The defender “strength” value, multiplied by the outcome of the defending dice is the damage defended by the defender. Whatever damage created by attacker which is in excess of the damage defended by the defender will reduce the “health” of the defender. Game ends when any players health reaches 0
 
 Player with lower health attacks first at the start of a match.
--- src/game.js
+++ src/game.js
@@ -1,11 +1,18 @@
 const Player = require("./player");
 
+// create Game class to manage the arean and combat.
+// Implement methods to handle the turn-based combat.
+// determine the starting player based on health.
+// Define rules for attacking ad defending
+// Hnadle the end game condition when a player's health reached zero.
+
 class Game {
   constructor(player1, player2) {
+    // Assign the first player object to the player1 property of this Game instance
     this.player1 = player1;
     this.player2 = player2;
   }
-
+  // Method to handle a single round of combat
   playRound() {
     let attacker, defender;
 
@@ -25,6 +32,7 @@ class Game {
       defender.receiveDamage(damageDealt);
     }
 
+    // Log the attack details and update the health of the defender
     console.log(
       `${attacker.name} attacks ${defender.name} for ${
         damageDealt > 0 ? damageDealt : 0
@@ -33,13 +41,16 @@ class Game {
     console.log(`${defender.name}'s health is now ${defender.health}.`);
   }
 
+  // Method to play the entire game until one player loses
   play() {
+    // Continue playing rounds as long as both players are alive
     while (this.player1.isAlive() && this.player2.isAlive()) {
       this.playRound();
     }
     this.declareWinner();
   }
 
+  // Method to declare the winner of the game
   declareWinner() {
     if (!this.player1.isAlive()) {
       console.log(`${this.player2.name} wins!`);
